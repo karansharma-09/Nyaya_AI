c0fd96d95063689074dbab897a6091265f8715d9
karansharma-09 <sharmakaran.kk09@gmail.com>
2026-03-08 13:41:40 +0530

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# PASTE YOUR REAL API KEY HERE
-genai.configure(api_key="AIzaSyAarftk48p_NKVycxEztqJidmG_yLKlr1E")
-
-print("Asking Google for your available models...")
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
